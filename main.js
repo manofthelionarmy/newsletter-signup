ff1de0e315816f975cff0ee23684b5c482ef9138
armando <armando@pop-os.localdomain>
2023-06-18 18:32:35 -0700

    corrected styling

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 
-document.getElementById('submit').addEventListener('click', (event) => {
+document.getElementById('signup-form').addEventListener('submit', (event) => {
   event.preventDefault()
-  location.href = "/success/"
+  // TODO: validate input and trigger error
+  document.getElementById('subscribe').classList.add('hidden')
+  document.getElementById('success').classList.remove('hidden')
+  document.getElementById('subscribe').classList.add('visible')
 })
 
-// setupCounter(document.querySelector('#counter'))
+document.getElementById('dismiss').addEventListener('click', (event) => {
+  document.getElementById('success').classList.add('hidden')
+  document.getElementById('subscribe').classList.remove('hidden')
+  document.getElementById('subscribe').classList.add('visible')
+})
